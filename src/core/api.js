17c22ff11180a6e91e59a9664b04631a9b35db62
Natalia Horbunova <nata.horbunova@gmail.com>
2023-08-31 01:09:52 +0300

    api base url corrected

--- src/core/api.js
+++ src/core/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://champion-store-server.onrender.com/1",
+    baseURL: "https://champion-store-server.onrender.com/",
   });
 
 const urlParams = {
